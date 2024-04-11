99b046d939347a8b7e48269efffe608a0b1a7ab8
Md Faizuddin <imdfaizuddin@gmail.com>
2024-04-11 23:43:25 +0530

    add use useState to store notes & add delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,34 @@ import { useState } from 'react'
 import Header from './Header'
 import Footer from "./Footer"
 import Note from "./Note"
+import CreateArea from './CreateArea'
 import notes from "./notes"
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, changeNotes] = useState([])
+  const [key, changeKey] = useState(0)
+  function addNote(title,note){
+    changeNotes((prev)=>
+      [...prev, {key: key, title: title, content: note}]
+    )
+    changeKey(key => key +=1)
+  }
+  function deleteNote(index){
+    changeNotes((prev)=> {
+       const data = prev.filter((item)=>{
+        return item.key != index;
+      })
+      return data;
+    })
+  }
 
   return (
     <>
       <Header />
-      {notes.map((note) =>
-        <Note
-          key={note.key}
-          id={note.key}
-          title={note.title}
-          content={note.content}
-        />
+      <CreateArea addNote={addNote}/>
+      {notes.map((note)=>
+      <Note key={note.key} id={note.key} title={note.title} content={note.content} del ={deleteNote} />
       )}
       <Footer />
     </>
