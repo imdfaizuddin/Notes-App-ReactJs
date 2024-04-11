c55cba9d62816c9cb2a5bfbba2e3bc2ee0ccd878
Md Faizuddin <imdfaizuddin@gmail.com>
2024-04-11 23:42:29 +0530

    add delete btn in notes component

--- src/Note.jsx
+++ src/Note.jsx
@@ -4,6 +4,7 @@ function Note(props){
     return <div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
+        <button className="del-btn" onClick={()=>{props.del(props.id)}}>DELETE</button>
     </div>
 }
 
