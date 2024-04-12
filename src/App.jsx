ccb18e57a1655c68c6472c560a9f067b9f4cb980
Md Faizuddin <imdfaizuddin@gmail.com>
2024-04-12 23:55:31 +0530

    add main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './Header'
 import Footer from "./Footer"
 import Note from "./Note"
 import CreateArea from './CreateArea'
-import notes from "./notes"
+// import notes from "./notes"
 import './App.css'
 
 function App() {
